c1a2e0be2664f41224468d5bef93a2ca1cbdefe0
Jéssica Espíndola <138414152+jessica-espindola@users.noreply.github.com>
2023-08-18 20:45:27 -0300

    Resolvendo proposta do primeiro dia do desafio
    reescrever o código abaixo de maneira que ele imprima as informações de maneira correta, que faça sentido e sem erros:
    
    let numeroUm = 1
    let stringUm = '1'
    let numeroTrinta = 30
    let stringTrinta = '30'
    let numeroDez = 10
    let stringDez = '10'
    
    if (COMPARAR O numeroUm e a stringUm) {
      console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
    } else {
      console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
    }
    
    if (COMPARAR O numeroTrinta e a stringTrinta) {
      console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
    } else {
      console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
    }
    
    if (COMPARAR O numeroDez e a stringDez) {
      console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
    } else {
      console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
    }

---
+++ day1.js
@@ -0,0 +1,24 @@
+let numeroUm = 1
+let stringUm = '1'
+let numeroTrinta = 30
+let stringTrinta = '30'
+let numeroDez = 10
+let stringDez = '10'
+
+if (typeof numeroUm == typeof stringUm) {
+  console.log ('As variáveis numeroUm e stringUm possuem os mesmos valores e são de tipos iguais.')
+} else {
+  console.log ('As variáveis numeroUm e stringUm possuem os mesmos valores, mas são de tipos diferentes.')
+}
+
+if (typeof numeroTrinta == typeof stringTrinta) {
+  console.log ('As variáveis numeroTrinta e stringTrinta possuem os mesmos valores e são de tipos iguais.')
+} else {
+  console.log ('As variáveis numeroTrinta e stringTrinta possuem os mesmos valores, mas são de tipos diferentes.')
+}
+
+if (typeof numeroDez == typeof stringDez) {
+  console.log ('As variáveis numeroDez e stringDez possuem os mesmos valores e são de tipos iguais.')
+} else {
+  console.log ('As variáveis numeroDez e stringDez possuem os mesmos valores, mas são de tipos diferentes.')
+}
