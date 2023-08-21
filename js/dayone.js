f72ce83bd522cdc6a45511e4de3b9f94f0f64d3b
Jéssica Espíndola <jessiicav.1@gmail.com>
2023-08-20 21:24:51 -0300

    atualizando a resposta da atividade do primeiro dia

--- js/dayone.js
+++ js/dayone.js
@@ -1,24 +1,40 @@
-let numeroUm = 1
-let stringUm = '1'
-let numeroTrinta = 30
-let stringTrinta = '30'
-let numeroDez = 10
-let stringDez = '10'
+let numeroUm = 1;
+let stringUm = '1';
+let numeroTrinta = 30;
+let stringTrinta = '30';
+let numeroDez = 10;
+let stringDez = '10';
 
-if (typeof numeroUm == typeof stringUm) {
-  console.log ('As variáveis numeroUm e stringUm possuem os mesmos valores e são de tipos iguais.')
-} else {
-  console.log ('As variáveis numeroUm e stringUm possuem os mesmos valores, mas são de tipos diferentes.')
+let secao = document.querySelector('.container__verifica-resposta-github');
+let secaoRespostaUm = document.querySelector('.container__verifica-resposta__conteudo-textoUm');
+let secaoRespostaDois = document.querySelector('.container__verifica-resposta__conteudo-textoDois');
+let secaoRespostaTres = document.querySelector('.container__verifica-resposta__conteudo-textoTres');
+const btnVerifica = document.querySelector('.botao-verificarEComparar');
+
+const secaoCodigo = document.querySelector('.container__verifica-resposta-github');
+
+btnVerifica.addEventListener("click", VerificaCompara);
+
+function VerificaCompara () {
+  if (typeof numeroUm == typeof stringUm) {
+    secaoRespostaUm.innerHTML = 'As variáveis numeroUm e stringUm possuem os mesmos valores e são de tipos iguais.';
+  } else {
+    secaoRespostaUm.innerHTML = 'As variáveis numeroUm e stringUm possuem os mesmos valores, mas são de tipos diferentes.';
+  }
+  
+  if (typeof numeroTrinta == typeof stringTrinta) {
+    secaoRespostaDois.innerHTML = 'As variáveis numeroTrinta e stringTrinta possuem os mesmos valores e são de tipos iguais.';
+  } else {
+    secaoRespostaDois.innerHTML = 'As variáveis numeroTrinta e stringTrinta possuem os mesmos valores, mas são de tipos diferentes.'
+  }
+  
+  if (typeof numeroDez == typeof stringDez) {
+    secaoRespostaTres.innerHTML = 'As variáveis numeroDez e stringDez possuem os mesmos valores e são de tipos iguais.'
+  
+  } else {
+    secaoRespostaTres.innerHTML = 'As variáveis numeroDez e stringDez possuem os mesmos valores, mas são de tipos diferentes.'
+  }
+
+  secaoCodigo.style.display = 'flex';
 }
 
-if (typeof numeroTrinta == typeof stringTrinta) {
-  console.log ('As variáveis numeroTrinta e stringTrinta possuem os mesmos valores e são de tipos iguais.')
-} else {
-  console.log ('As variáveis numeroTrinta e stringTrinta possuem os mesmos valores, mas são de tipos diferentes.')
-}
-
-if (typeof numeroDez == typeof stringDez) {
-  console.log ('As variáveis numeroDez e stringDez possuem os mesmos valores e são de tipos iguais.')
-} else {
-  console.log ('As variáveis numeroDez e stringDez possuem os mesmos valores, mas são de tipos diferentes.')
-}
